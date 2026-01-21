c1a1ad1626b65e25cc8d39ec5d656a28ade5711f
Ferennia Putri <146501383+Ferennia25@users.noreply.github.com>
2026-01-21 15:30:47 +0700

    Update DataBaseConnection.java

--- JAVA_SQL/java/DataBaseConnection.java
+++ JAVA_SQL/java/DataBaseConnection.java
@@ -4,7 +4,7 @@ import java.sql.DriverManager;
 public class DataBaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/payment_bank";
     private static final String USER = "root";
-    private static final String PASSWORD = "ferennia25";
+    private static final String PASSWORD = "***";
 
     public static Connection connect() throws Exception {
         return DriverManager.getConnection(URL,USER,PASSWORD);
